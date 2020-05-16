05f15043d40fa82e2be4add5e3ebe3df15ecddfe
sabas1080 <s@electroniccats.com>
2020-05-16 11:24:13 -0500

    add agroboard

---
+++ boards/agroboard/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats"
+#define PRODUCT_NAME "AgroBoard"
+#define VOLUME_LABEL "AGROBOARD"
+#define INDEX_URL "https://electroniccats.com"
+
+#define BOARD_ID "SAMD21G18A-agro-board-v0"
+
+#define USB_VID 0x03EB   // Atmel
+#define USB_PID 0x2402   // Generic HID device
+
+#define LED_PIN PIN_PB22
+
+#endif
