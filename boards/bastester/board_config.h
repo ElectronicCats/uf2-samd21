a561d2fc05addb6850244bba4d39a7d7a981e290
sabas1080 <sabasjimenez@gmail.com>
2020-11-20 10:40:59 -0600

    init bootloader bast test

---
+++ boards/bastester/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "Bast Tester"
+#define VOLUME_LABEL "BASTEST"
+#define INDEX_URL "https://electroniccats.com/"
+#define BOARD_ID "SAMD21E18A-Bast-Test-V1"
+
+#define USB_VID 0x1209
+#define USB_PID 0xBA10
+
+#define LED_PIN PIN_PA18
+
+#endif
