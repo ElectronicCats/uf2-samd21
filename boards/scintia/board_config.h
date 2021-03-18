67d6fed99887747217681cddb0b16fd90fdf961e
sabas1080 <sabasjimenez@gmail.com>
2021-03-17 20:13:29 -0600

    Added scintia

---
+++ boards/scintia/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Scintia"
+#define PRODUCT_NAME "LAMPthermal"
+#define VOLUME_LABEL "LAMP"
+#define INDEX_URL "http://electroniccats.com"
+#define BOARD_ID "lampthermal"
+
+#define USB_VID 0x04D8
+#define USB_PID 0xE988
+
+#define LED_PIN PIN_PA27
+
+#endif
