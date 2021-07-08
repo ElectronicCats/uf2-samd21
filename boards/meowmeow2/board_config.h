bde072d09de3e74d2736554ec8a623fe9bec9b56
sabas1080 <sabasjimenez@gmail.com>
2021-07-08 13:07:50 -0500

    Add Meow Meow 2
    Yes!

---
+++ boards/meowmeow2/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats"
+#define PRODUCT_NAME "Meow Meow 2"
+#define VOLUME_LABEL "MEOWMEOW2"
+#define INDEX_URL "https://electroniccats.com/howtomeow"
+#define BOARD_ID "SAMD21G18A-Meow-Meow-2"
+
+#define USB_VID 0x1209   // http://pid.codes/
+#define USB_PID 0xBAB1   // Meow Meow PID
+
+#define LED_PIN PIN_PA23
+
+#endif
